f2c49fd699d9873ba7aba80631a03a8e0ca2fa69
wangpj <1344246287@qq.com>
2020-08-13 22:08:49 +0800

    更新版本

--- crawler4py/setup.py
+++ crawler4py/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="crawler4py",
-    version="0.3",
+    version="0.4",
     author="winner-hub",
     author_email="1344246287@qq.com",
     description=("A distributed crawler framework based on Python"),
