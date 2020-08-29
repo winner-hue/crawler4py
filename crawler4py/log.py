b8fb589f292ca9dedd6ac129f4959b24815e504e
wangpj <1344246287@qq.com>
2020-08-30 06:59:27 +0800

    更新日志

--- crawler4py/log.py
+++ crawler4py/log.py
@@ -1,31 +1,33 @@
 import json
 import logging
 import logging.config
+import loguru
 
-log_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "simple": {
-            "format": "%(lineno)d  %(asctime)s %(threadName)s--%(filename)s--%(funcName)s--%(levelname)s--%(message)s"
-        }
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "level": "DEBUG",
-            "formatter": "simple",
-            "stream": "ext://sys.stdout"
-        }
-    },
-    "loggers": {
-        "py_crawler": {
-            "level": "INFO",
-            "handlers": ["console"],
-            "propagate": "no"
-        }
-    }
-}
+
+# log_config = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "formatters": {
+#         "simple": {
+#             "format": "%(lineno)d  %(asctime)s %(threadName)s--%(filename)s--%(funcName)s--%(levelname)s--%(message)s"
+#         }
+#     },
+#     "handlers": {
+#         "console": {
+#             "class": "logging.StreamHandler",
+#             "level": "DEBUG",
+#             "formatter": "simple",
+#             "stream": "ext://sys.stdout"
+#         }
+#     },
+#     "loggers": {
+#         "py_crawler": {
+#             "level": "INFO",
+#             "handlers": ["console"],
+#             "propagate": "no"
+#         }
+#     }
+# }
 
 
 class Logger(object):
@@ -38,10 +40,11 @@ class Logger(object):
             if setting.get("logger_path"):
                 with open(setting.get("logger_path"), "r", encoding="utf-8") as f:
                     config = json.load(f)
+                    logging.config.dictConfig(config)
+                    Logger.logger = logging.getLogger("py_crawler")
+                    Logger.logger.info("日志实例化成功")
             else:
-                config = log_config
-            logging.config.dictConfig(config)
-            Logger.logger = logging.getLogger("py_crawler")
-            Logger.logger.info("日志实例化成功")
+                Logger.logger = loguru.logger
+                Logger.logger.info("日志实例化成功")
             cls.__instance = Logger()
         return cls.__instance
